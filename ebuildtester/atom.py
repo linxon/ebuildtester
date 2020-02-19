62f89cb000980cb5296d1be0a21cf3333b1be9e6
Yury Martynov <email@linxon.ru>
2020-02-19 14:01:26 +0300

    fix errors while atom parsing

--- ebuildtester/atom.py
+++ ebuildtester/atom.py
@@ -1,33 +1,28 @@
+import gentoopm
+
 class AtomException(Exception):
     pass
-
 
 class Atom(object):
 
     def __init__(self, atom):
         # We expect an atom of the form [=]CATEGORY/PACKAGE[-VERSION].
+        self.atom = atom
         self.category = None
         self.package = None
         self.version = None
 
-        # We don't store the optional '='.
-        temp = atom.split("=")
-        self.atom = temp[-1]
-
         try:
-            self.category, self.package = self.atom.split("/")
-        except ValueError:
-            raise AtomException(
-                "ATOM has to be of the form [=]SECTION/PACKAGE[-VERSION]")
+            pm = gentoopm.get_package_manager()
 
-        # Split off version.
-        try:
-            temp = self.package.index("-")
-            if temp > -1:
-                self.version = self.package[temp + 1:]
-                self.package = self.package[:temp]
+            self.category = pm.Atom(self.atom).key.category
+            self.package = pm.Atom(self.atom).key.package
+            self.version = pm.Atom(self.atom).version
+
+            if self.category is None or self.package is None:
+                raise ValueError
         except ValueError:
-            pass
+            raise AtomException("ATOM has to be of the form [=]SECTION/PACKAGE[-VERSION]")
 
     def __str__(self):
         if self.version is not None:
