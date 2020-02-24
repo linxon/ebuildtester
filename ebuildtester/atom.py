b5787eb2eb34bbf6514202a719d391d6133fe5d1
Yury Martynov <email@linxon.ru>
2020-02-24 10:35:42 +0300

    new Atom implementation

--- ebuildtester/atom.py
+++ ebuildtester/atom.py
@@ -1,40 +1,88 @@
-import gentoopm
+import re
+
+from portage.dbapi.dep_expand import dep_expand
+from portage.versions import cpv_getversion
+import portage.exception as pe
+
 
 class AtomException(Exception):
     pass
 
+
 class Atom(object):
 
     def __init__(self, atom):
-        # We expect an atom of the form [=]CATEGORY/PACKAGE[-VERSION].
-        self.atom = atom
-        self.category = None
-        self.package = None
-        self.version = None
-
         try:
-            pm = gentoopm.get_package_manager()
+            # We expect an atom of the form [=]CATEGORY/PACKAGE[-VERSION].
+            self.atom = dep_expand(str(atom))
+        except pe.InvalidAtom:
+            raise AtomException(
+                "ATOM has to be of the form [=]SECTION/PACKAGE[-VERSION]")
 
-            self.category = pm.Atom(self.atom).key.category
-            self.package = pm.Atom(self.atom).key.package
-            self.version = pm.Atom(self.atom).version
+        self.category = self.atomCategory()
+        self.package = self.atomName()
+        self.version = self.atomVersion()
+        self.repo = self.atomRepo()
 
-            if self.category is None or self.package is None:
-                raise ValueError
-        except ValueError:
-            raise AtomException("ATOM has to be of the form [=]SECTION/PACKAGE[-VERSION]")
+    def _splitPackage(self):
+        return self.atom.split("/", 1)
+
+    def atomCategory(self):
+        """Returns the package category without name"""
+        category = re.sub('^(=|<=|>=|<|>)', '', self._splitPackage()[0])
+        if category == 'null':
+            raise AtomException(
+                "ATOM has to be of the form [=]SECTION/PACKAGE[-VERSION]")
+
+        return category
+
+    def atomName(self):
+        """Returns the package name without category"""
+        pkg = self._splitPackage()[1]
+        suffix = ['-' + str(self.atomVersion()),
+                  '::' + str(self.atomRepo())]
+        for s in suffix:
+            pkg = pkg.replace(s, '')
+
+        return pkg
+
+    def atomVersion(self):
+        """Returns the package version"""
+
+        return cpv_getversion(self.atom)
+
+    def atomRepo(self):
+        """Returns the package repository"""
+        pkg = self._splitPackage()[1].split("::", 1)
+        if len(pkg) == 2:
+            return pkg[1]
+
+    def atomCatName(self):
+        """Returns the package category and name without version"""
+        return "/".join([self.category, self.package])
+
+    def atomComplete(self):
+        """Returns a portage compatible string representation"""
+        suff = []
+        pref = ''
+        if self.version is not None:
+            suff += ['-' + self.version]
+            pref = '='
+
+        if self.repo is not None:
+            suff += ['::' + self.repo]
+
+        return (pref + self.category + '/' + self.package +
+                ''.join([str(s) for s in suff]))
 
     def __str__(self):
-        if self.version is not None:
-            prefix = "="
-            suffix = "-" + self.version
-        else:
-            prefix = ""
-            suffix = ""
-        return prefix + self.category + "/" + self.package + suffix
+        return self.atomComplete()
 
     def __eq__(self, other):
-        result = (self.atom == other.atom)
+        result = (self.category == other.category
+                  and self.package == other.package
+                  and self.version == other.version)
+
         return result
 
     def __repr__(self):
