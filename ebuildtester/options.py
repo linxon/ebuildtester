4ea317242d4c414eff72f5f45db3769e2678154f
Yury Martynov <email@linxon.ru>
2020-06-19 18:02:00 +0300

    ebuildtester/options.py: add some env params for ccache

--- ebuildtester/options.py
+++ ebuildtester/options.py
@@ -23,10 +23,17 @@ def init():
 
     sh_env = []
 
-    # man make.conf
-    exec_env = ["ACCEPT_KEYWORDS", "ACCEPT_LICENSE", "PORTDIR", "CCACHE_DIR", "PORTAGE_BINHOST"]
+    # man 5 make.conf
+    exec_env = ["ACCEPT_KEYWORDS", "ACCEPT_LICENSE", "PORTDIR", "PORTAGE_TMPDIR", "PORTAGE_BINHOST"]
     exec_env += ["USE", "MAKEOPTS", "FEATURES", "PKGDIR", "DISTDIR", "EMERGE_DEFAULT_OPTS"]
     exec_env += ["CFLAGS", "CXXFLAGS", "LDFLAGS", "CPU_FLAGS_X86"]
+    # man 1 ccache
+    exec_env += ["CCACHE_DIR", "CCACHE_DISABLE", "CCACHE_COMPILER", "CCACHE_COMPILERCHECK"]
+    exec_env += ["CCACHE_COMPRESS", "CCACHE_NOCOMPRESS", "CCACHE_COMPRESSLEVEL", "CCACHE_EXTENSION"]
+    exec_env += ["CCACHE_DEPEND", "CCACHE_NODEPEND", "CCACHE_EXTRAFILES", "CCACHE_DIRECT"]
+    exec_env += ["CCACHE_HARDLINK", "CCACHE_NOHARDLINK", "CCACHE_HASHDIR", "CCACHE_NOHASHDIR"]
+    exec_env += ["CCACHE_MAXSIZE", "CCACHE_PATH", "CCACHE_READONLY", "CCACHE_NOREADONLY"]
+    exec_env += ["CCACHE_STATS", "CCACHE_NOSTATS", "CCACHE_TEMPDIR", "CCACHE_UMASK", "CCACHE_NLEVELS"]
 
     options = None
     log = logging.getLogger("test-package")
